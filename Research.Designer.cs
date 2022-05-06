63cebd6f1a46777a15327aa7186b08faf827a6f9
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-06 21:51:41 +0200

    Added Research basic UI

--- Research.Designer.cs
+++ Research.Designer.cs
@@ -30,6 +30,22 @@ namespace Quantum_World
         private void InitializeComponent()
         {
             this.lblResearch = new System.Windows.Forms.Label();
+            this.lblResearch1 = new System.Windows.Forms.Label();
+            this.lblResearch2 = new System.Windows.Forms.Label();
+            this.lblResearch3 = new System.Windows.Forms.Label();
+            this.lblResearch4 = new System.Windows.Forms.Label();
+            this.lblResearch5 = new System.Windows.Forms.Label();
+            this.lblResearch6 = new System.Windows.Forms.Label();
+            this.lblResearch7 = new System.Windows.Forms.Label();
+            this.lblResearch8 = new System.Windows.Forms.Label();
+            this.btnResearch1Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch2Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch3Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch4Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch5Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch6Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch7Upgrade = new System.Windows.Forms.Button();
+            this.btnResearch8Upgrade = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblResearch
@@ -41,10 +57,178 @@ namespace Quantum_World
             this.lblResearch.TabIndex = 0;
             this.lblResearch.Text = "You are in research";
             // 
+            // lblResearch1
+            // 
+            this.lblResearch1.AutoSize = true;
+            this.lblResearch1.Location = new System.Drawing.Point(90, 98);
+            this.lblResearch1.Name = "lblResearch1";
+            this.lblResearch1.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch1.TabIndex = 1;
+            this.lblResearch1.Text = "lblResearch1";
+            // 
+            // lblResearch2
+            // 
+            this.lblResearch2.AutoSize = true;
+            this.lblResearch2.Location = new System.Drawing.Point(202, 97);
+            this.lblResearch2.Name = "lblResearch2";
+            this.lblResearch2.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch2.TabIndex = 2;
+            this.lblResearch2.Text = "lblResearch2";
+            // 
+            // lblResearch3
+            // 
+            this.lblResearch3.AutoSize = true;
+            this.lblResearch3.Location = new System.Drawing.Point(300, 98);
+            this.lblResearch3.Name = "lblResearch3";
+            this.lblResearch3.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch3.TabIndex = 3;
+            this.lblResearch3.Text = "lblResearch3";
+            // 
+            // lblResearch4
+            // 
+            this.lblResearch4.AutoSize = true;
+            this.lblResearch4.Location = new System.Drawing.Point(412, 98);
+            this.lblResearch4.Name = "lblResearch4";
+            this.lblResearch4.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch4.TabIndex = 4;
+            this.lblResearch4.Text = "lblResearch4";
+            // 
+            // lblResearch5
+            // 
+            this.lblResearch5.AutoSize = true;
+            this.lblResearch5.Location = new System.Drawing.Point(89, 250);
+            this.lblResearch5.Name = "lblResearch5";
+            this.lblResearch5.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch5.TabIndex = 5;
+            this.lblResearch5.Text = "lblResearch5";
+            // 
+            // lblResearch6
+            // 
+            this.lblResearch6.AutoSize = true;
+            this.lblResearch6.Location = new System.Drawing.Point(201, 250);
+            this.lblResearch6.Name = "lblResearch6";
+            this.lblResearch6.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch6.TabIndex = 6;
+            this.lblResearch6.Text = "lblResearch6";
+            // 
+            // lblResearch7
+            // 
+            this.lblResearch7.AutoSize = true;
+            this.lblResearch7.Location = new System.Drawing.Point(299, 249);
+            this.lblResearch7.Name = "lblResearch7";
+            this.lblResearch7.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch7.TabIndex = 7;
+            this.lblResearch7.Text = "lblResearch7";
+            // 
+            // lblResearch8
+            // 
+            this.lblResearch8.AutoSize = true;
+            this.lblResearch8.Location = new System.Drawing.Point(411, 249);
+            this.lblResearch8.Name = "lblResearch8";
+            this.lblResearch8.Size = new System.Drawing.Size(69, 13);
+            this.lblResearch8.TabIndex = 8;
+            this.lblResearch8.Text = "lblResearch8";
+            // 
+            // btnResearch1Upgrade
+            // 
+            this.btnResearch1Upgrade.Location = new System.Drawing.Point(83, 114);
+            this.btnResearch1Upgrade.Name = "btnResearch1Upgrade";
+            this.btnResearch1Upgrade.Size = new System.Drawing.Size(75, 48);
+            this.btnResearch1Upgrade.TabIndex = 9;
+            this.btnResearch1Upgrade.Text = "Upgrade";
+            this.btnResearch1Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch1Upgrade.Click += new System.EventHandler(this.BtnResearch1Upgrade_Click);
+            // 
+            // btnResearch2Upgrade
+            // 
+            this.btnResearch2Upgrade.Location = new System.Drawing.Point(195, 113);
+            this.btnResearch2Upgrade.Name = "btnResearch2Upgrade";
+            this.btnResearch2Upgrade.Size = new System.Drawing.Size(75, 49);
+            this.btnResearch2Upgrade.TabIndex = 10;
+            this.btnResearch2Upgrade.Text = "Upgrade";
+            this.btnResearch2Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch2Upgrade.Click += new System.EventHandler(this.BtnResearch2Upgrade_Click);
+            // 
+            // btnResearch3Upgrade
+            // 
+            this.btnResearch3Upgrade.Location = new System.Drawing.Point(294, 114);
+            this.btnResearch3Upgrade.Name = "btnResearch3Upgrade";
+            this.btnResearch3Upgrade.Size = new System.Drawing.Size(75, 48);
+            this.btnResearch3Upgrade.TabIndex = 11;
+            this.btnResearch3Upgrade.Text = "Upgrade";
+            this.btnResearch3Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch3Upgrade.Click += new System.EventHandler(this.BtnResearch3Upgrade_Click);
+            // 
+            // btnResearch4Upgrade
+            // 
+            this.btnResearch4Upgrade.Location = new System.Drawing.Point(405, 114);
+            this.btnResearch4Upgrade.Name = "btnResearch4Upgrade";
+            this.btnResearch4Upgrade.Size = new System.Drawing.Size(75, 48);
+            this.btnResearch4Upgrade.TabIndex = 12;
+            this.btnResearch4Upgrade.Text = "Upgrade";
+            this.btnResearch4Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch4Upgrade.Click += new System.EventHandler(this.BtnResearch4Upgrade_Click);
+            // 
+            // btnResearch5Upgrade
+            // 
+            this.btnResearch5Upgrade.Location = new System.Drawing.Point(83, 266);
+            this.btnResearch5Upgrade.Name = "btnResearch5Upgrade";
+            this.btnResearch5Upgrade.Size = new System.Drawing.Size(75, 49);
+            this.btnResearch5Upgrade.TabIndex = 13;
+            this.btnResearch5Upgrade.Text = "Upgrade";
+            this.btnResearch5Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch5Upgrade.Click += new System.EventHandler(this.BtnResearch5Upgrade_Click);
+            // 
+            // btnResearch6Upgrade
+            // 
+            this.btnResearch6Upgrade.Location = new System.Drawing.Point(195, 266);
+            this.btnResearch6Upgrade.Name = "btnResearch6Upgrade";
+            this.btnResearch6Upgrade.Size = new System.Drawing.Size(75, 49);
+            this.btnResearch6Upgrade.TabIndex = 14;
+            this.btnResearch6Upgrade.Text = "Upgrade";
+            this.btnResearch6Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch6Upgrade.Click += new System.EventHandler(this.BtnResearch6Upgrade_Click);
+            // 
+            // btnResearch7Upgrade
+            // 
+            this.btnResearch7Upgrade.Location = new System.Drawing.Point(294, 266);
+            this.btnResearch7Upgrade.Name = "btnResearch7Upgrade";
+            this.btnResearch7Upgrade.Size = new System.Drawing.Size(75, 49);
+            this.btnResearch7Upgrade.TabIndex = 15;
+            this.btnResearch7Upgrade.Text = "Upgrade";
+            this.btnResearch7Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch7Upgrade.Click += new System.EventHandler(this.BtnResearch7Upgrade_Click);
+            // 
+            // btnResearch8Upgrade
+            // 
+            this.btnResearch8Upgrade.Location = new System.Drawing.Point(405, 266);
+            this.btnResearch8Upgrade.Name = "btnResearch8Upgrade";
+            this.btnResearch8Upgrade.Size = new System.Drawing.Size(75, 49);
+            this.btnResearch8Upgrade.TabIndex = 16;
+            this.btnResearch8Upgrade.Text = "Upgrade";
+            this.btnResearch8Upgrade.UseVisualStyleBackColor = true;
+            this.btnResearch8Upgrade.Click += new System.EventHandler(this.BtnResearch8Upgrade_Click);
+            // 
             // Research
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnResearch8Upgrade);
+            this.Controls.Add(this.btnResearch7Upgrade);
+            this.Controls.Add(this.btnResearch6Upgrade);
+            this.Controls.Add(this.btnResearch5Upgrade);
+            this.Controls.Add(this.btnResearch4Upgrade);
+            this.Controls.Add(this.btnResearch3Upgrade);
+            this.Controls.Add(this.btnResearch2Upgrade);
+            this.Controls.Add(this.btnResearch1Upgrade);
+            this.Controls.Add(this.lblResearch8);
+            this.Controls.Add(this.lblResearch7);
+            this.Controls.Add(this.lblResearch6);
+            this.Controls.Add(this.lblResearch5);
+            this.Controls.Add(this.lblResearch4);
+            this.Controls.Add(this.lblResearch3);
+            this.Controls.Add(this.lblResearch2);
+            this.Controls.Add(this.lblResearch1);
             this.Controls.Add(this.lblResearch);
             this.Name = "Research";
             this.Size = new System.Drawing.Size(575, 391);
@@ -56,5 +240,21 @@ namespace Quantum_World
         #endregion
 
         private System.Windows.Forms.Label lblResearch;
+        private System.Windows.Forms.Label lblResearch1;
+        private System.Windows.Forms.Label lblResearch2;
+        private System.Windows.Forms.Label lblResearch3;
+        private System.Windows.Forms.Label lblResearch4;
+        private System.Windows.Forms.Label lblResearch5;
+        private System.Windows.Forms.Label lblResearch6;
+        private System.Windows.Forms.Label lblResearch7;
+        private System.Windows.Forms.Label lblResearch8;
+        private System.Windows.Forms.Button btnResearch1Upgrade;
+        private System.Windows.Forms.Button btnResearch2Upgrade;
+        private System.Windows.Forms.Button btnResearch3Upgrade;
+        private System.Windows.Forms.Button btnResearch4Upgrade;
+        private System.Windows.Forms.Button btnResearch5Upgrade;
+        private System.Windows.Forms.Button btnResearch6Upgrade;
+        private System.Windows.Forms.Button btnResearch7Upgrade;
+        private System.Windows.Forms.Button btnResearch8Upgrade;
     }
 }
--- Research.cs
+++ Research.cs
@@ -16,5 +16,38 @@ namespace Quantum_World
         {
             InitializeComponent();
         }
+
+        private void BtnResearch1Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch1Upgrade.Text = "Research 1 Upgraded";
+        }
+        private void BtnResearch2Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch2Upgrade.Text = "Research 2 Upgraded";
+        }
+        private void BtnResearch3Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch3Upgrade.Text = "Research 3 Upgraded";
+        }
+        private void BtnResearch4Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch4Upgrade.Text = "Research 4 Upgraded";
+        }
+        private void BtnResearch5Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch5Upgrade.Text = "Research 5 Upgraded";
+        }
+        private void BtnResearch6Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch6Upgrade.Text = "Research 6 Upgraded";
+        }
+        private void BtnResearch7Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch7Upgrade.Text = "Research 7 Upgraded";
+        }
+        private void BtnResearch8Upgrade_Click(object sender, EventArgs e)
+        {
+            btnResearch8Upgrade.Text = "Research 8 Upgraded";
+        }
     }
 }
